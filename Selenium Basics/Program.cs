952534be3f2ea06b84cd9ad307279cbbf8e9ba0b
Sandwip Roy <sandwip31@gmail.com>
2020-04-12 17:31:25 +0530

    initial set up of selenium

---
+++ Selenium Basics/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Selenium_Basics
+{
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
+    using OpenQA.Selenium.Firefox;
+    using System;
+    using System.Threading;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //initializing the web driver
+            IWebDriver chromeDriver = new ChromeDriver();
+            //browsing the website using URL
+            chromeDriver.Navigate().GoToUrl("https://gmail.com");
+            //searching for the textbox to enter username
+            var userName = chromeDriver.FindElement(By.CssSelector("#identifierId"));
+            //typing in the user name
+            userName.SendKeys("<email address>");
+            // clicking the next button to go to the password's page
+            chromeDriver.FindElement(By.XPath("//div[@role='button']")).Click();
+            //searching for the textbox to enter password
+            var password = chromeDriver.FindElement(By.XPath("//input[@type='password']"), 100);
+            //typing the password
+            password.SendKeys("<password>");
+            //selecting the checkbox in the email
+            chromeDriver.FindElement(By.XPath("//div[@id='passwordNext']"), 2).Click();
+            var checkBox = chromeDriver.FindElement(By.XPath("//div[@id=':31']"), 10);
+            checkBox.Click();
+            chromeDriver.Quit();
+        }
+    }
+}
---
+++ Selenium Basics/WebDriverExtensions.cs
@@ -0,0 +1,41 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Selenium_Basics
+{
+    public static class WebDriverExtensions
+    {
+        public static IWebElement FindElement(this IWebDriver driver, By by, int timeoutInSeconds)
+        {
+            if (timeoutInSeconds > 0)
+            {
+                for (int i = 0; i < timeoutInSeconds * 1000; i++)
+                {
+                    try
+                    {
+                        bool isFieldDisplayed = driver.FindElement(by).Displayed;
+                        if (isFieldDisplayed)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            Thread.Sleep(1);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Thread.Sleep(1);
+                    }
+                }
+            }
+            return driver.FindElement(by);
+        }
+    }
+}
